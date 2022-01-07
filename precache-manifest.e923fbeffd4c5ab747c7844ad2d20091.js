76605449c9066611b27e7518d6e8ccc3aee729c7
Yahia Essam <yahia_essam@siliconexpert.com>
2022-01-07 20:23:53 +0200

    Updates

---
+++ precache-manifest.e923fbeffd4c5ab747c7844ad2d20091.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb83ce3795d8ee76179d",
+    "url": "/static/css/main.06a85e0b.chunk.css"
+  },
+  {
+    "revision": "fb83ce3795d8ee76179d",
+    "url": "/static/js/main.fb83ce37.chunk.js"
+  },
+  {
+    "revision": "e9809bb8a5088691a045",
+    "url": "/static/js/1.e9809bb8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2690ac534751793d12ae508bf06fea3",
+    "url": "/static/media/check-mark.c2690ac5.svg"
+  },
+  {
+    "revision": "6ed01e4ca242a4bdf993da775f5e562c",
+    "url": "/static/media/cancel.6ed01e4c.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "322e6795f4883dda589ad2a618552a4d",
+    "url": "/static/media/three-dots.322e6795.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "12e32680cabbdf112c8054b7a5fade6b",
+    "url": "/index.html"
+  }
+];
