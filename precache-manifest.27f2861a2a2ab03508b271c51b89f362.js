7f56b1892c8d5559d8bb26e043016c389815c644
Yahia Essam <yahia_essam@siliconexpert.com>
2022-01-07 22:19:04 +0200

    Updates

---
+++ precache-manifest.27f2861a2a2ab03508b271c51b89f362.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7df0c27b654a4e66f525",
+    "url": "/react-project-myreads/static/css/main.c59540d8.chunk.css"
+  },
+  {
+    "revision": "7df0c27b654a4e66f525",
+    "url": "/react-project-myreads/static/js/main.7df0c27b.chunk.js"
+  },
+  {
+    "revision": "fe3ddac0e04a9f79c44f",
+    "url": "/react-project-myreads/static/js/1.fe3ddac0.chunk.js"
+  },
+  {
+    "revision": "007d459c69e077327f9d",
+    "url": "/react-project-myreads/static/js/runtime~main.007d459c.js"
+  },
+  {
+    "revision": "c2690ac534751793d12ae508bf06fea3",
+    "url": "/react-project-myreads/static/media/check-mark.c2690ac5.svg"
+  },
+  {
+    "revision": "6ed01e4ca242a4bdf993da775f5e562c",
+    "url": "/react-project-myreads/static/media/cancel.6ed01e4c.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/react-project-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/react-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "322e6795f4883dda589ad2a618552a4d",
+    "url": "/react-project-myreads/static/media/three-dots.322e6795.svg"
+  },
+  {
+    "revision": "797375d61be5063ed394f7cb16a5cdfa",
+    "url": "/react-project-myreads/index.html"
+  }
+];
